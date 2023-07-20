cdfae15cc2a3bbe7a1231477d1745943fe945c06
syshinkr <syshinkr228@gmail.com>
2023-07-20 13:22:34 +0900

    주문 서비스 개발

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,74 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    /**
+     * 주문
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+        // 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // 배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        // 주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장
+        // 원래 delivery, orderItem 둘 다 persist 해야하지만
+        // ==CascadeType.ALL== 덕에 order 만 persist 해도 delivery, orderItem 도 persist 된다
+        // cascade 사용 범위는 주인이 private owner인 경우가 권장
+        // - order만이 delivery, orderItems 를 관리함 (owner)
+        // - 다른 것이 참조 못하는 private 상태
+        // 위 조건이 만족되지 않으면 별도로 전용 repository 생성해야 함
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        // 주문 취소
+        order.cancel();
+
+        // sql 직접짜는 경우, 엔티티 상태가 바뀌면 다 일일이 DB에 업데이트문을 써야하지만
+        // JPA 는 이를 감지하여 알아서 업데이트 해줌
+    }
+
+    // 검색
+//    public List<Order> findOrders(OrderSearch orderSearch) {
+//        return orderRepository.findAll(orderSearch);
+//    }
+}
